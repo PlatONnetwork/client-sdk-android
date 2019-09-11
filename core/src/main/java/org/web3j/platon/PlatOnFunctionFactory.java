e48ef8594a8b6cf4a3f0238f6b47b6aba5d3f0ec
xiajufeng <xiajufeng@juzix.net>
2019-09-11 14:12:27 +0800

    修复gasPrice预估不准的bug

---
+++ core/src/main/java/org/web3j/platon/PlatOnFunctionFactory.java
@@ -0,0 +1,51 @@
+package org.web3j.platon;
+
+import org.web3j.abi.datatypes.BytesType;
+import org.web3j.abi.datatypes.generated.Uint16;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.abi.datatypes.generated.Uint64;
+import org.web3j.utils.Numeric;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class PlatOnFunctionFactory {
+
+
+    /**
+     * 创建转账实例
+     * @return
+     */
+    public static PlatOnFunction createTransferFunction() {
+        return new PlatOnFunction(FunctionType.TRANSFER_FUNC_TYPE);
+    }
+
+    /**
+     * 创建委托实例
+     * @param nodeId
+     * @param stakingAmountType
+     * @param amount
+     * @return
+     */
+    public static PlatOnFunction createDelegateFunction(String nodeId, StakingAmountType stakingAmountType, BigInteger amount) {
+        return new PlatOnFunction(FunctionType.DELEGATE_FUNC_TYPE,
+                Arrays.asList(new Uint16(stakingAmountType.getValue())
+                        , new BytesType(Numeric.hexStringToByteArray(nodeId))
+                        , new Uint256(amount)));
+    }
+
+    /**
+     * 创建解除委托实例
+     * @param nodeId
+     * @param stakingBlockNum
+     * @param amount
+     * @return
+     */
+    public static PlatOnFunction createUnDelegateFunction(String nodeId, BigInteger stakingBlockNum, BigInteger amount) {
+        return new PlatOnFunction(FunctionType.WITHDREW_DELEGATE_FUNC_TYPE,
+                Arrays.asList(new Uint64(stakingBlockNum)
+                        , new BytesType(Numeric.hexStringToByteArray(nodeId))
+                        , new Uint256(amount)));
+    }
+
+}
